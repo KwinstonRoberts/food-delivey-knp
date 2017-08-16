547eb3a98ccfb20c476788174b17082e26c7e090
kyle roberts <kyle.w.r@hotmail.com>
2017-08-16 20:15:40 +0000

    added menu details

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.cta').on('click','button',function(){
+    $('.cta').slideUp();
+    $('#menu').slideDown();
+  })
+
+  $('.options>button').on('click',function(e){
+    $(this).toggleClass('active');
+  })
+
+})
