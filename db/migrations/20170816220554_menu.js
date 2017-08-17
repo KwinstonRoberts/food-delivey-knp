10512f1b63e1aa4e9940425e3955a3b74dfaae89
pennygibson <cenneboulet@yahoo.ca>
2017-08-17 18:10:51 +0000

    add images

--- db/migrations/20170816220554_menu.js
+++ db/migrations/20170816220554_menu.js
@@ -1,11 +1,4 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('dishes', function (table) {
-    table.increments();
-    table.string('name');
-    table.string('pic');
-    table.float('price');
-    table.string('description');
-  });
 };
 
 exports.down = function(knex, Promise) {
