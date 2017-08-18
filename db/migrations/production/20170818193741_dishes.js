26eb0de1133abeee83940fcc39d87184d98045f7
kyle roberts <kyle.w.r@hotmail.com>
2017-08-18 19:53:41 +0000

    added twilio routes

--- db/migrations/production/20170818193741_dishes.js
+++ db/migrations/production/20170818193741_dishes.js
@@ -1,11 +1,12 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('menu_cart', function (table) {
+  return knex.schema.createTable('dishes', function (table) {
      table.increments('id');
      table.string('name');
      table.string('pic').
      table.float('price');
      table.string('type');
+   });
 };
 
 exports.down = function(knex, Promise) {
