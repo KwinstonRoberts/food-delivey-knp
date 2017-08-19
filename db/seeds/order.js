08a883b70db4ce534712831960197c6076a9aaf1
kyle roberts <kyle.w.r@hotmail.com>
2017-08-19 20:05:05 +0000

    added new sms functions

--- db/seeds/order.js
+++ db/seeds/order.js
@@ -1,9 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('order').del()
-
-      return Promise.all([
-      ]);
-    });
+    knex('order').del();
+    return Promise.all([
+  ]);
 };
