a4717e971ad467bfdc39dbf39c9d908952cafaf1
kyle roberts <kyle.w.r@hotmail.com>
2017-08-19 03:31:28 +0000

    voice function

--- server.js
+++ server.js
@@ -64,8 +64,6 @@ app.post("/order", (req, res) => {
         // Use the Twilio Node.js SDK to build an XML response
         const twiml = new VoiceResponse();
         twiml.say({ voice: 'alice' }, 'hello world!');
-        // Render the response as XML in reply to the webhook request
-        response.type('text/xml');
         return twiml.toString();
       },
       to: '+16477619205',
