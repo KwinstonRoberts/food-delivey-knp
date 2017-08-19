8376634842492fc96c9807fa2bf70cf94fa33fcb
kyle roberts <kyle.w.r@hotmail.com>
2017-08-19 18:51:47 +0000

    fixed knex attribute typo

--- db/migrations/production/20170819001516_order.js
+++ db/migrations/production/20170819001516_order.js
@@ -3,7 +3,7 @@ exports.up = function(knex, Promise) {
   return promise.all([
      knex.schema.createTable('order', function (table) {
        table.increments('id');
-       table.text('name');
+       table.string('name');
        table.foreign('email');
        table.integer('phone');
        table.text('receipt','longtext');
