227e9de290e428b9166162908baeda3cebe3564b
kyle roberts <kyle.w.r@hotmail.com>
2017-08-19 19:46:09 +0000

    removed email field

--- db/migrations/production/20170819194247_order.js
+++ db/migrations/production/20170819194247_order.js
@@ -1,9 +1,6 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-      knex.schema.table('order', function (table) {
-      table.dropColumn('email');
-    }),
      knex.schema.table('order', function (table) {
        table.string('email');
      })
